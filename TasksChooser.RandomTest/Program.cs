29eb231c1670b7335ed6ec28aeefa27ae694e7c1
Petr Voborník <vobornik@outlook.cz>
2019-02-04 12:22:19 +0100

    RandomTest Binary

--- TasksChooser.RandomTest/Program.cs
+++ TasksChooser.RandomTest/Program.cs
@@ -15,8 +15,46 @@ namespace TasksChooser.RandomTest
             string mainSeed = (args?.Length??0) > 0 ? args[0] : "TasksChooser.RandomTest";
             int contOfColumns = (args?.Length ?? 0) > 1 ? Convert.ToInt32(args[1]) : 100;
             int contOfIRows = (args?.Length ?? 0) > 2 ? Convert.ToInt32(args[2]) : 100000;
-            string fileName = $"{DateTime.Now:yyyy-MM-dd-HH-mm-ss}.txt";
+            string fileName = $"{DateTime.Now:yyyy-MM-dd-HH-mm-ss}";
 
+            CreateBinaryData(mainSeed, contOfColumns, contOfIRows, fileName+".bin");
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static void CreateBinaryData(string mainSeed, int contOfColumns, int contOfRows, string fileName)
+        {
+            var rndForSeeds = new TaskRandom(mainSeed);
+            var rnds = new TaskRandom[contOfColumns];
+
+            using (var stream = File.Create(fileName))
+            using (BinaryWriter writer = new BinaryWriter(stream))
+            {
+                // 1st row = seeds
+                for (int col = 0; col < contOfColumns; col++)
+                {
+                    string seed = rndForSeeds.NextDouble().ToString("N17").Substring(2);
+                    rnds[col] = new TaskRandom(seed);
+                    writer.Write(seed);
+                }
+
+                // numbers
+                int tenPercent = contOfRows / 10;
+                for (int row = 0; row < contOfRows; row++)
+                {
+                    for (int col = 0; col < contOfColumns; col++)
+                        writer.Write(rnds[col].NextDouble());
+                    if (row % tenPercent == 0)
+                        Console.WriteLine("{0:N0}%", 100 * row / (double)contOfRows);
+                }
+            }
+            Console.WriteLine("{0:N0}%", 100);
+        }
+
+
+        static void CreateTextData(string mainSeed, int contOfColumns, int contOfRows, string fileName)
+        {
             var rndForSeeds = new TaskRandom(mainSeed);
             var rnds = new TaskRandom[contOfColumns];
 
@@ -27,24 +65,22 @@ namespace TasksChooser.RandomTest
                 {
                     string seed = rndForSeeds.NextDouble().ToString("N17").Substring(2);
                     rnds[col] = new TaskRandom(seed);
-                    writer.Write(seed+";");
+                    writer.Write(seed + ";");
                 }
                 writer.WriteLine();
 
                 // numbers
-                int tenPercent = contOfIRows / 10;
-                for (int row = 0; row < contOfIRows; row++)
+                int tenPercent = contOfRows / 10;
+                for (int row = 0; row < contOfRows; row++)
                 {
                     for (int col = 0; col < contOfColumns; col++)
                         writer.Write(rnds[col].NextDouble().ToString("N15") + ";");
                     writer.WriteLine();
                     if (row % tenPercent == 0)
-                        Console.WriteLine("{0:N0}%", 100 * row / (double)contOfIRows);
+                        Console.WriteLine("{0:N0}%", 100 * row / (double)contOfRows);
                 }
             }
-
-            Console.WriteLine("Done");
-            Console.ReadLine();
+            Console.WriteLine("{0:N0}%", 100);
         }
     }
 }
